6b6bab7c47720344791db00863d8b41697b132f6
Louis <ubc@louismau.me>
2016-06-01 16:56:24 -0700

    Added print statements to test event handling

--- src/com/muhhost/permissionshop/listeners/InventoryListener.java
+++ src/com/muhhost/permissionshop/listeners/InventoryListener.java
@@ -2,12 +2,17 @@ package com.muhhost.permissionshop.listeners;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.*;
+import org.bukkit.event.EventHandler;
 
 public class InventoryListener implements Listener{
 
+
+    @EventHandler
     public void onClick(InventoryClickEvent click)
     {
-
+        System.out.println("Hello using event");
+        System.out.print(click.getSlot());
+        click.setCancelled(true);
     }
 
 
