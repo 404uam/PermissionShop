b792adbf79a18bc8d135fd66c084691e38f8dd37
Louis <ubc@louismau.me>
2016-06-01 16:45:50 -0700

    Added Action listener

---
+++ src/com/muhhost/permissionshop/listeners/InventoryListener.java
@@ -0,0 +1,14 @@
+package com.muhhost.permissionshop.listeners;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.*;
+
+public class InventoryListener implements Listener{
+
+    public void onClick(InventoryClickEvent click)
+    {
+
+    }
+
+
+}
--- src/com/muhhost/permissionshop/main/PermissionShop.java
+++ src/com/muhhost/permissionshop/main/PermissionShop.java
@@ -1,5 +1,6 @@
 package com.muhhost.permissionshop.main;
 
+import com.muhhost.permissionshop.listeners.InventoryListener;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,6 +10,7 @@ public class PermissionShop extends JavaPlugin {
     private FileConfiguration config = super.getConfig();
 
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new InventoryListener(),this);
         this.getCommand("shop").setExecutor(new ShopCommand());
     }
 
